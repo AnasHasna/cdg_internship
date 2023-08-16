4abb83184a1dd404c992e04690afe8b7f850a93b
Anas Hasna <hasnaanas230@gmail.com>
2023-08-16 15:17:43 +0100

    configure private routes

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -5,10 +5,14 @@ const {
   forgetPassword,
   resetPassword,
 } = require("../controllers/userController");
+const verifyToken = require("../middleware/authMiddleware");
 const userRoutes = express.Router();
 
 userRoutes.post("/register", registerUser);
 userRoutes.post("/auth", authUser);
 userRoutes.route("/forgetpassword").post(forgetPassword).put(resetPassword);
+userRoutes.get("/welcome", verifyToken, (req, res) => {
+  res.status(200).send("Welcome to the CDG API");
+});
 
 module.exports = userRoutes;
